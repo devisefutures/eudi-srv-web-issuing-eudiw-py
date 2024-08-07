0d4748938627074cc20458550ad2d30e09b40834
José Miranda <jose.miranda@devisefutures.com>
2024-08-07 12:41:02 +0100

    Configuração para AMA - PT

--- app/app_config/config_countries.py
+++ app/app_config/config_countries.py
@@ -33,34 +33,33 @@ class ConfCountries:
     formCountry = "FC"
     # supported countries
     supported_countries = {
-        "EU": {
-            "name": "nodeEU",
-            "pid_url_oidc": cfgserv.service_url + "eidasnode/lightrequest?country=EU",
-            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_EU.pem",
-            # "pid_mdoc_privkey": 'app\certs\PID-DS-0001_EU.pem',
-            "pid_mdoc_privkey_passwd": None,  # None or bytes,
-            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_EU_cert.der",
-            "loa": "http://eidas.europa.eu/LoA/high",
-            "supported_credentials": [
-                "eu.europa.ec.eudi.pid_mdoc",
-                "eu.europa.ec.eudi.pid_jwt_vc_json",
-            ],
-            "custom_modifiers": {
-                "family_name": "CurrentFamilyName",
-                "given_name": "CurrentGivenName",
-                "birth_date": "DateOfBirth",
-            },
-            "connection_type": "eidasnode",
-            "dynamic_R2": cfgserv.service_url + "eidasnode/dynamic_R2",
-        },
+        # "EU": {
+        #     "name": "nodeEU",
+        #     "pid_url_oidc": cfgserv.service_url + "eidasnode/lightrequest?country=EU",
+        #     "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_EU.pem",
+        #     # "pid_mdoc_privkey": 'app\certs\PID-DS-0001_EU.pem',
+        #     "pid_mdoc_privkey_passwd": None,  # None or bytes,
+        #     "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_EU_cert.der",
+        #     "loa": "http://eidas.europa.eu/LoA/high",
+        #     "supported_credentials": [
+        #         "eu.europa.ec.eudi.pid_mdoc",
+        #         "eu.europa.ec.eudi.pid_jwt_vc_json",
+        #     ],
+        #     "custom_modifiers": {
+        #         "family_name": "CurrentFamilyName",
+        #         "given_name": "CurrentGivenName",
+        #         "birth_date": "DateOfBirth",
+        #     },
+        #     "connection_type": "eidasnode",
+        #     "dynamic_R2": cfgserv.service_url + "eidasnode/dynamic_R2",
+        # },
         formCountry: {
-            "name": "FormEU",
+            "name": "FormPT",
             "pid_url": cfgserv.service_url + "pid/form",
-            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_UT.pem",
-            # "pid_mdoc_privkey": 'app\certs\PID-DS-0001_UT.pem',
-            "pid_mdoc_privkey_passwd": None,  # None or bytes
-            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_UT_cert.der",
-            "un_distinguishing_sign": "FC",
+            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0005.pid-ds-0002.key.pem",
+            "pid_mdoc_privkey_passwd": b"pid-ds-0002",  # None or bytes
+            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0005.cert.der",
+            "un_distinguishing_sign": "P",
             "supported_credentials": [
                 "eu.europa.ec.eudi.pid_mdoc",
                 "eu.europa.ec.eudi.pid_jwt_vc_json",
@@ -75,10 +74,9 @@ class ConfCountries:
         },
         "PT": {
             "name": "Portugal",
-            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_PT.pem",
-            # "pid_mdoc_privkey": 'app\certs\PID-DS-0001_PT.pem',
-            "pid_mdoc_privkey_passwd": None,  # None or bytes
-            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_PT_cert.der",
+            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0005.pid-ds-0002.key.pem",
+            "pid_mdoc_privkey_passwd": b"pid-ds-0002",  # None or bytes
+            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0005.cert.der",
             "un_distinguishing_sign": "P",
             "supported_credentials": [
                 "eu.europa.ec.eudi.pid_mdoc",
@@ -90,7 +88,7 @@ class ConfCountries:
             "connection_type": "oauth",
             "oidc_auth": {
                 "url": "https://preprod.autenticacao.gov.pt/oauth/askauthorization?",
-                "redirect_uri": "https://preprod.issuer.eudiw.dev/"
+                "redirect_uri": "https://eudiw-issuer.ama.projj.eu/"
                 + "dynamic/redirect",
                 "scope": {
                     "eu.europa.ec.eudi.pid.1": {
@@ -118,87 +116,87 @@ class ConfCountries:
                 "custom_modifiers": "",
             },
         },
-        "EE": {
-            "name": "Estonia",
-            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_EE.pem",
-            # "pid_mdoc_privkey": 'app\certs\PID-DS-0001_EE.pem',
-            "pid_mdoc_privkey_passwd": None,  # None or bytes
-            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_EE_cert.der",
-            "supported_credentials": [
-                "eu.europa.ec.eudi.pid_mdoc",
-                "eu.europa.ec.eudi.pid_jwt_vc_json",
-            ],
-            "connection_type": "openid",
-            "oidc_auth": {
-                "base_url": "https://tara-test.ria.ee",
-                "redirect_uri": urlReturnEE,
-                "scope": "openid",
-                "state": "hkMVY7vjuN7xyLl5",
-                "response_type": "code",
-                "client_id": "eu_europa_ec_eudiw_pid_provider_1_ppr",
-            },
-            "attribute_request": {
-                "header": {"Host": "tara-test.ria.ee"},
-                "custom_modifiers": {
-                    "birth_date": "date_of_birth",
-                },
-            },
-            "oidc_redirect": {
-                "headers": {
-                    "Host": "tara-test.ria.ee",
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": "Basic ZXVfZXVyb3BhX2VjX2V1ZGl3X3BpZF9wcm92aWRlcl8xX3BwcjpINUVpVjdPaGZMTUs1TFBvcXB0NG5WT1FJeEdicEZ3MQ==",
-                },
-                "grant_type": "authorization_code",
-                "redirect_uri": "https://pprpid.provider.eudiw.projj.eu/tara/redirect",
-            },
-        },
-        "CZ": {
-            "name": "Czechia",
-            "pid_url_oidc": cfgserv.service_url + "eidasnode/lightrequest?country=CZ",
-            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_CZ.pem",
-            # "pid_mdoc_privkey": 'app\certs\PID-DS-0001_CZ.pem',
-            "pid_mdoc_privkey_passwd": None,  # None or bytes
-            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_CZ_cert.der",
-            "loa": "http://eidas.europa.eu/LoA/high",
-            "supported_credentials": [
-                "eu.europa.ec.eudi.pid_mdoc",
-                "eu.europa.ec.eudi.pid_jwt_vc_json",
-            ],
-            "connection_type": "eidasnode",
-            "dynamic_R2": cfgserv.service_url + "eidasnode/dynamic_R2",
-        },
-        "NL": {
-            "name": "Netherland",
-            "pid_url_oidc": cfgserv.service_url + "eidasnode/lightrequest?country=NL",
-            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_NL.pem",
-            "pid_mdoc_privkey_passwd": None,  # None or bytes
-            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_NL_cert.der",
-            "loa": "http://eidas.europa.eu/LoA/high",
-            "supported_credentials": [
-                "eu.europa.ec.eudi.pid_mdoc",
-                "eu.europa.ec.eudi.pid_jwt_vc_json",
-            ],
-            "connection_type": "eidasnode",
-            "dynamic_R2": cfgserv.service_url + "eidasnode/dynamic_R2",
-        },
-        "LU": {
-            "name": "Luxembourg",
-            "pid_url_oidc": cfgserv.service_url + "eidasnode/lightrequest?country=LU",
-            "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_LU.pem",
-            "pid_mdoc_privkey_passwd": None,  # None or bytes
-            "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_LU_cert.der",
-            "loa": "http://eidas.europa.eu/LoA/high",
-            "supported_credentials": [
-                "eu.europa.ec.eudi.pid_mdoc",
-                "eu.europa.ec.eudi.pid_jwt_vc_json",
-            ],
-            "custom_modifiers": {
-                "family_name": "CurrentFamilyName",
-                "given_name": "CurrentGivenName",
-                "birth_date": "DateOfBirth",
-            },
-            "connection_type": "eidasnode",
-            "dynamic_R2": cfgserv.service_url + "eidasnode/dynamic_R2",
-        },
+        # "EE": {
+        #     "name": "Estonia",
+        #     "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_EE.pem",
+        #     # "pid_mdoc_privkey": 'app\certs\PID-DS-0001_EE.pem',
+        #     "pid_mdoc_privkey_passwd": None,  # None or bytes
+        #     "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_EE_cert.der",
+        #     "supported_credentials": [
+        #         "eu.europa.ec.eudi.pid_mdoc",
+        #         "eu.europa.ec.eudi.pid_jwt_vc_json",
+        #     ],
+        #     "connection_type": "openid",
+        #     "oidc_auth": {
+        #         "base_url": "https://tara-test.ria.ee",
+        #         "redirect_uri": urlReturnEE,
+        #         "scope": "openid",
+        #         "state": "hkMVY7vjuN7xyLl5",
+        #         "response_type": "code",
+        #         "client_id": "eu_europa_ec_eudiw_pid_provider_1_ppr",
+        #     },
+        #     "attribute_request": {
+        #         "header": {"Host": "tara-test.ria.ee"},
+        #         "custom_modifiers": {
+        #             "birth_date": "date_of_birth",
+        #         },
+        #     },
+        #     "oidc_redirect": {
+        #         "headers": {
+        #             "Host": "tara-test.ria.ee",
+        #             "Content-Type": "application/x-www-form-urlencoded",
+        #             "Authorization": "Basic ZXVfZXVyb3BhX2VjX2V1ZGl3X3BpZF9wcm92aWRlcl8xX3BwcjpINUVpVjdPaGZMTUs1TFBvcXB0NG5WT1FJeEdicEZ3MQ==",
+        #         },
+        #         "grant_type": "authorization_code",
+        #         "redirect_uri": "https://pprpid.provider.eudiw.projj.eu/tara/redirect",
+        #     },
+        # },
+        # "CZ": {
+        #     "name": "Czechia",
+        #     "pid_url_oidc": cfgserv.service_url + "eidasnode/lightrequest?country=CZ",
+        #     "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_CZ.pem",
+        #     # "pid_mdoc_privkey": 'app\certs\PID-DS-0001_CZ.pem',
+        #     "pid_mdoc_privkey_passwd": None,  # None or bytes
+        #     "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_CZ_cert.der",
+        #     "loa": "http://eidas.europa.eu/LoA/high",
+        #     "supported_credentials": [
+        #         "eu.europa.ec.eudi.pid_mdoc",
+        #         "eu.europa.ec.eudi.pid_jwt_vc_json",
+        #     ],
+        #     "connection_type": "eidasnode",
+        #     "dynamic_R2": cfgserv.service_url + "eidasnode/dynamic_R2",
+        # },
+        # "NL": {
+        #     "name": "Netherland",
+        #     "pid_url_oidc": cfgserv.service_url + "eidasnode/lightrequest?country=NL",
+        #     "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_NL.pem",
+        #     "pid_mdoc_privkey_passwd": None,  # None or bytes
+        #     "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_NL_cert.der",
+        #     "loa": "http://eidas.europa.eu/LoA/high",
+        #     "supported_credentials": [
+        #         "eu.europa.ec.eudi.pid_mdoc",
+        #         "eu.europa.ec.eudi.pid_jwt_vc_json",
+        #     ],
+        #     "connection_type": "eidasnode",
+        #     "dynamic_R2": cfgserv.service_url + "eidasnode/dynamic_R2",
+        # },
+        # "LU": {
+        #     "name": "Luxembourg",
+        #     "pid_url_oidc": cfgserv.service_url + "eidasnode/lightrequest?country=LU",
+        #     "pid_mdoc_privkey": "/etc/eudiw/pid-issuer/privkey/PID-DS-0001_LU.pem",
+        #     "pid_mdoc_privkey_passwd": None,  # None or bytes
+        #     "pid_mdoc_cert": "/etc/eudiw/pid-issuer/cert/PID-DS-0001_LU_cert.der",
+        #     "loa": "http://eidas.europa.eu/LoA/high",
+        #     "supported_credentials": [
+        #         "eu.europa.ec.eudi.pid_mdoc",
+        #         "eu.europa.ec.eudi.pid_jwt_vc_json",
+        #     ],
+        #     "custom_modifiers": {
+        #         "family_name": "CurrentFamilyName",
+        #         "given_name": "CurrentGivenName",
+        #         "birth_date": "DateOfBirth",
+        #     },
+        #     "connection_type": "eidasnode",
+        #     "dynamic_R2": cfgserv.service_url + "eidasnode/dynamic_R2",
+        # },
     }
